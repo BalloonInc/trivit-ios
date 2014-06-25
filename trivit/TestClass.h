8afefffcc64c1e9309b6882433c2b5dc61045b77
Pieterjan Criel <pieterjan.criel@ugent.be>
2014-06-26 00:33:27 +0200

    adding test tiles

---
+++ trivit/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  trivit
+//
+//  Created by Pieterjan Criel on 26/06/14.
+//  Copyright (c) 2014 Balloon Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ trivit/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  trivit
+//
+//  Created by Pieterjan Criel on 26/06/14.
+//  Copyright (c) 2014 Balloon Inc. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
