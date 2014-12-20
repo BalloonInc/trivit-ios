79d0ce7d850985910f9c4ce442a4be7530884748
Wouter <wouter@Wouters-Mac.local>
2014-12-20 06:21:56 +0600

    No more warnings. Let's check for updates of rest kit for iOS 8 if they come.

--- Pods/RestKit/Code/ObjectMapping/RKHTTPUtilities.m
+++ Pods/RestKit/Code/ObjectMapping/RKHTTPUtilities.m
@@ -340,12 +340,11 @@ static const char _httpDate_eof_actions[] = {
 };
 
 static NSDate *_parseHTTPDate(const char *buf, size_t bufLen) {
+    
     const char *p = buf, *pe = p + bufLen, *eof = pe;
     int parsed = 0, cs = 1;
     NSDate *date = NULL;
-    
-    CFGregorianDate gdate;
-    memset(&gdate, 0, sizeof(CFGregorianDate));
+    NSDateComponents *gdate;
     
     {
         int _slen, _trans;
@@ -401,9 +400,10 @@ static NSDate *_parseHTTPDate(const char *buf, size_t bufLen) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{ gmtTimeZone = CFTimeZoneCreateWithTimeIntervalFromGMT(NULL, 0.0); });
     
-    if(parsed == 1) { date = [NSDate dateWithTimeIntervalSinceReferenceDate:CFGregorianDateGetAbsoluteTime(gdate, gmtTimeZone)]; }
-    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    if(parsed == 1) { date = [calendar dateFromComponents:gdate];}
     return(date);
+
 }
 
 /*
