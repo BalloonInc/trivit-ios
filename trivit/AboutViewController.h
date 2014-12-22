129600270f3a201f166a63328efbfbca301692ba
Wouter <wouter@Wouters-Mac.local>
2014-12-22 06:19:12 +0600

    everything in a scrollview, edit scrollview size when keyboard is shown, return button for textview/textfields

--- trivit/AboutViewController.h
+++ trivit/AboutViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AboutViewController : UIViewController
+@interface AboutViewController : UIViewController <UITextViewDelegate, UITextFieldDelegate>
 
 @end
--- trivit/AboutViewController.m
+++ trivit/AboutViewController.m
@@ -12,21 +12,34 @@
 
 
 @interface AboutViewController ()
+// smiley button properties
 @property (weak, nonatomic) IBOutlet UIButton *highestScoreButton;
 @property (weak, nonatomic) IBOutlet UIButton *highScoreButton;
 @property (weak, nonatomic) IBOutlet UIButton *mediumScoreButton;
 @property (weak, nonatomic) IBOutlet UIButton *lowScoreButton;
 @property (weak, nonatomic) IBOutlet UIButton *lowestScoreButton;
+
+// static label properties
 @property (weak, nonatomic) IBOutlet UILabel *feedBackLabel;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *rightBarButton;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
+// Logic properties
 @property (nonatomic) NSInteger score;
-
 @property (strong, nonatomic) NSArray *feedbackTexts;
+
+//user text properties
 @property (weak, nonatomic) IBOutlet UITextView *feedbackDetail;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
 
 @end
 
 @implementation AboutViewController
+
+#pragma mark - Constants
+int const DONEBUTTON = 0;
+int const SENDBUTTON = 1;
 
 -(NSArray *)feedbackTexts
 {
@@ -39,13 +52,23 @@
              ];
 }
 
+#pragma mark viewDidLoad stuff
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self setScoreActive:0];
     [self layoutViews];
+    [self setPlaceHolderTextForTextView:self.feedbackDetail];
 
-    // Do any additional setup after loading the view.
+    self.rightBarButton.tag = SENDBUTTON;
+    
+    self.feedbackDetail.delegate = self;
+    self.nameField.delegate = self;
+    self.emailField.delegate = self;
+    
+    // subscribe to notifications for keyboard show and hide, used for changing view size
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
 }
 
 -(void) layoutViews
@@ -61,15 +84,19 @@
     self.feedbackDetail.layer.shadowRadius = 1.0f;
 }
 
-
-- (IBAction)scoreButtonPressed:(id)sender
+#pragma mark bar button behavior
+- (IBAction)barButtonPressed:(id)sender
 {
-    UIButton *senderButton = (UIButton*) sender;
-    
-    [self setScoreActive:senderButton.tag];
+    UIBarButtonItem *button = (UIBarButtonItem*) sender;
+    if (button.tag==DONEBUTTON)
+        [self doneEditingDetailTextView];
+    else if (button.tag==SENDBUTTON)
+        [self sendFeedback];
+    else
+        NSLog(@"error, button not correctly labeled");
 }
 
-- (IBAction)sendButtonPressed:(id)sender
+- (void) sendFeedback
 {
     NSString * iOSVersion = [[UIDevice currentDevice] systemVersion];
     NSString * modelDevice = [[UIDevice currentDevice] model];
@@ -78,7 +105,14 @@
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-
+// logic concerning the smileys
+- (IBAction)scoreButtonPressed:(id)sender
+{
+    [self doneEditingDetailTextView];
+    UIButton *senderButton = (UIButton*) sender;
+    
+    [self setScoreActive:senderButton.tag];
+}
 
 -(void) setScoreActive:(NSInteger)score
 {
@@ -94,5 +128,68 @@
     self.feedBackLabel.text = self.feedbackTexts[score];
     self.feedBackLabel.textColor=(score>0)?[UIColor darkGrayColor]:[UIColor blackColor];
 }
+
+// Dismiss keyboard for UITextView
+- (void)doneEditingDetailTextView
+{
+    [self.feedbackDetail resignFirstResponder];
+    [self.nameField resignFirstResponder];
+    [self.emailField resignFirstResponder];
+    
+    self.rightBarButton.title = NSLocalizedString(@"Send", @"Send button for feedback");
+    self.rightBarButton.tag = SENDBUTTON;
+}
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    self.rightBarButton.title = NSLocalizedString(@"Done", @"Done button right top when editing text view");
+    self.rightBarButton.tag = DONEBUTTON;
+    if(textView.textColor==[UIColor grayColor]){
+        textView.textColor=[UIColor blackColor];
+        textView.text=@"";
+    }
+}
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    if(!textView.text.length){
+        [self setPlaceHolderTextForTextView: textView];
+    }
+}
+-(void) setPlaceHolderTextForTextView: (UITextView *)textView
+{
+    textView.textColor=[UIColor grayColor];
+    textView.text=NSLocalizedString(@"Your feedback", @"Placeholder text for feedback text");
+}
+
+// Dismiss keyboard for UITextfields
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return NO;
+}
+
+// resize view if keyboard is shown/hidden
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(self.scrollView.contentInset.top, self.scrollView.contentInset.left, kbSize.height, self.scrollView.contentInset.right);
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+    
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    // restore the insets
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(self.scrollView.contentInset.top, self.scrollView.contentInset.left, 0, self.scrollView.contentInset.right);
+    
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+}
+
 
 @end
