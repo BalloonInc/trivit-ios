692583f5c4576b1b999018ea9010ef80bae7b78a
Wouter Devriendt <wouterdevriendt@gmail.com>
2015-01-06 08:51:34 +0000

    MainListViewController.m edited online with Bitbucket more examples

--- trivit/MainListViewController.m
+++ trivit/MainListViewController.m
@@ -64,7 +64,7 @@ int const OUTSIDE_TAP = 3;
 
 -(NSString *) trivitExampleNameAtIndex: (NSInteger)index{
     if (!self.placeholderTrivitTitles){
-        NSString *fruits = NSLocalizedString(@"Days in prison,Sport cars owned,Days without holiday,Cups of coffee this year,Tequilla shots today,Pretty girls in the bar,Days of work left for our app,Days without cursing,People stalked,Unfinished Netflix movies,Tallies added,iPhones owned,Bugs in our software,Beers,Pairs of shoes owned,Rainy days in 2015,Glasses of water,",@"Placeholder texts for newly added trivits,Stars in the sky");
+        NSString *fruits = NSLocalizedString(@"Days in prison,Sport cars owned,Days without holiday,Cups of coffee this year,Tequilla shots today,Pretty girls in the bar,Days of work left for our app,Days without cursing,People stalked,Unfinished Netflix movies,Tallies added,iPhones owned,Bugs in our software,Beers,Pairs of shoes owned,Rainy days in 2015,Glasses of water,Stars in the sky,Days waiting for Apple Watch release",@"Placeholder texts for newly added trivits");
         self.placeholderTrivitTitles = [fruits componentsSeparatedByString:@","];
     }
     return self.placeholderTrivitTitles[index%self.placeholderTrivitTitles.count];
